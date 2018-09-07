b5bf2d0be719747599dae60cff7c22955160bf33
Wimski <wimreckman@gmail.com>
2018-09-07 16:02:36 +0200

    Added doc block to FileParserInterface

--- src/DataImport/FileParser/FileParserInterface.php
+++ src/DataImport/FileParser/FileParserInterface.php
@@ -4,5 +4,9 @@ namespace App\DataImport\FileParser;
 
 interface FileParserInterface
 {
+    /**
+     * @param string $data
+     * @return array
+     */
     public function parse(string $data): array;
 }
