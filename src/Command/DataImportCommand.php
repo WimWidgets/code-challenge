6e86aad7de5a13a2ac23b9c858c7f7d4e48cd1c6
Wimski <wimreckman@gmail.com>
2018-09-07 13:45:58 +0200

    Updated data import command

--- src/Command/DataImportCommand.php
+++ src/Command/DataImportCommand.php
@@ -2,15 +2,25 @@
 
 namespace App\Command;
 
+use App\DataImport\Importer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpFoundation\File\File;
 
 class DataImportCommand extends Command
 {
-    public function __construct()
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
     {
+        $this->em = $em;
         parent::__construct();
     }
 
@@ -19,13 +29,20 @@ class DataImportCommand extends Command
         $this
             ->setName('challenge:data-import')
             ->setDescription('Starts the data import for the code challenge.')
-            ->addArgument('file', InputArgument::REQUIRED, 'Relative path to the data file.')
+            ->addArgument('file', InputArgument::REQUIRED, 'Absolute path or url to the data file.')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $file = $input->getArgument('file');
-        //@TODO: do import
+        $io = new SymfonyStyle($input, $output);
+        $file = new File($input->getArgument('file'));
+        try {
+            $importer = new Importer($this->em, $file);
+            $importer->import();
+            $io->success('Your data file has been successfully imported!');
+        } catch (\Exception $e) {
+            $io->error($e->getMessage());
+        }
     }
 }
